6f253e85cf33ca0f0127258c53131ccaefc00963
takuma <k023g0015@g.neec.ac.jp>
2024-07-08 13:06:53 +0900

    #include <wrl.h>をincludeした

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@
 #include <cmath>
 #include <fstream>
 #include <sstream>
+#include <wrl.h>
 #include "externals/imgui/imgui.h"
 #include "externals/imgui/imgui_impl_dx12.h"
 #include "externals/imgui/imgui_impl_win32.h"
